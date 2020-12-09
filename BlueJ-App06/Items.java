d96579e4660ed1a0eb9c125ca7f1dcb1dfe40a30
Tyronne282 <bradburntyronne@gmail.com>
2020-12-09 14:23:52 +0000

    added player and items

---
+++ BlueJ-App06/Items.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Items - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Items
+{
+    KEY, POTION, SWORD, SHIELD, TRAPS
+}
---
+++ BlueJ-App06/Player.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    private int health; 
+    private int energy;
+    private ArrayList itemList;
+    private int attackValue;
+    private int defenceValue;
+    
+}
